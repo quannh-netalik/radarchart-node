8c8dd723ca0b76f016545da9f0209fc7155e474d
quannh.netalik <quannh.netalik@gmail.com>
2020-12-16 21:55:21 +0700

    Fix catch error

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Radar Chart
 
 ![Status](https://img.shields.io/badge/build-passing-brightgreen)
-![Version](https://img.shields.io/badge/version-1.0.3-lightgrey)
+![Version](https://img.shields.io/badge/version-1.0.4-lightgrey)
 ![Size](https://img.shields.io/badge/size-152KB-green)
 ![Chart](https://img.shields.io/badge/chart-d3-blue)
 ![License](https://img.shields.io/badge/license-MIT-green)
--- utils/index.js
+++ utils/index.js
@@ -13,20 +13,29 @@ exports.generateRadarChart = (d3n, type = 'svg', {
     dest: _dest = FILE_DESTINATION,
     fileName: _fileName = FILE_NAME
 } = {}) => { 
-    // convert to svg & html
-    const svgString = d3n.svgString();
-    const html = d3n.html();
+    try {
+        // convert to svg & html
+        const svgString = d3n.svgString();
+        const html = d3n.html();
 
-    // get the data to write & return
-    const data = type === 'html' ? html : svgString;
+        // get the data to write & return
+        const data = type === 'html' ? html : svgString;
 
-    // get file path
-    const file = getFilePath(_dest, _fileName, type);
-    if (_write) fs.writeFile(`${file}`, data, function () {
-        console.log(`=======>> Exported "${file}", open in a web browser`);
-    });
+        // get file path
+        const file = getFilePath(_dest, _fileName, type);
+        if (_write) fs.writeFile(`${file}`, data, function (err) {
+            if (err) {
+                console.error('ERROR!', err)
+                process.exit(1)
+            }
+            console.log(`=======>> Exported "${file}", open in a web browser`);
+        });
 
-    return data;
+        return data;
+    } catch(_e) {
+        console.error('ERROR!', _e)
+        process.exit(1)
+    }
 };
 
 exports.generateRadarImage = (d3n, type = 'png', {
@@ -36,29 +45,44 @@ exports.generateRadarImage = (d3n, type = 'png', {
     dest: _dest = FILE_DESTINATION,
     fileName: _fileName = FILE_NAME
 } = { }) => {
-    const viewport = _width && _height ? { _width, _height } : false;
-    const html = d3n.html();
-    
-    const screenShotOptions = {
-        viewport,
-        path: getFilePath(_dest, _fileName, type),
-        _quality,
-        type
-    };
+    try {
+        const viewport = _width && _height ? { _width, _height } : false;
+        const html = d3n.html();
+        
+        const screenShotOptions = {
+            viewport,
+            path: getFilePath(_dest, _fileName, type),
+            _quality,
+            type
+        };
 
-    return puppeteer.launch()
-        .then((browser) => {
-            browser.newPage()
-                .then((page) => {
-                    page.setContent(html)
-                    if (viewport) {
-                        page.setViewport(viewport);
-                    }
-                    page.screenshot(screenShotOptions)
-                        .then(() => browser.close())
-                        .then(() => console.log('=======>> Exported:', screenShotOptions.path))
-                        .catch(console.error);
-                })
-        })
-        .catch(console.error);
+        return puppeteer.launch()
+            .then((browser) => {
+                browser.newPage()
+                    .then((page) => {
+                        page.setContent(html)
+                        if (viewport) {
+                            page.setViewport(viewport);
+                        }
+                        page.screenshot(screenShotOptions)
+                            .then(() => browser.close())
+                            .then(() => console.log('=======>> Exported:', screenShotOptions.path))
+                            .catch(err => {
+                                console.error('ERROR!', err)
+                                process.exit(1)
+                            });
+                    }) 
+                    .catch(err => {
+                        console.error('ERROR!', err)
+                        process.exit(1)
+                    });
+            })
+            .catch(err => {
+                console.error('ERROR!', err)
+                process.exit(1)
+            });
+    } catch(_e) {
+        console.error('ERROR!', _e)
+        process.exit(1)
+    }
 };
